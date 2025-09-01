014cb5a8ffdb00a6a6065330c49fa8aa8968de18
laurawada <laura.wada@germinare.org.br>
2025-09-01 08:00:10 -0300

    feat: carrossel

--- app.js
+++ app.js
@@ -16,3 +16,21 @@ closeBtn.addEventListener('click', function() {
     modal.classList.remove("open-modal")
 
 })
+
+/* Carrossel feito com tutorial do Web Dev Simplified: https://www.youtube.com/watch?v=9HcxHDS2w1s&t=270s */
+const buttons = document.querySelectorAll("[data-carousel-button]")
+
+buttons.forEach(button => {
+    button.addEventListener("click", () => {
+        const offset = button.dataset.carouselButton === "proximo" ? 1 : -1;
+        const slides = button.closest("section").querySelector("[data-slides]")
+
+        const activeSlide = slides.querySelector("[data-active]")
+        let newIndex = [...slides.children].indexOf(activeSlide) + offset;
+        if (newIndex < 0) newIndex = slides.children.length - 1;
+        if (newIndex >= slides.children.length) newIndex = 0;
+
+        slides.children[newIndex].dataset.active = true;
+        delete activeSlide.dataset.active;
+    })
+});
